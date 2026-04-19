8d344b8bb3d6862d26f41d9f24f79aaece69c5cc
Anuj Tiwari <143748818+Linchpin22@users.noreply.github.com>
2026-04-19 18:57:44 +0530

    added pricing

--- src/Data/Constant.js
+++ src/Data/Constant.js
@@ -1,7 +1,7 @@
 export const Constant = {
   Company: {
     phones: ["+919621037777", "+918881970855"],
-    email: "brandbuzz2210@gmail.com",
+    email: "info@blackgiraffe.in",
     whatsapp: "919621037777",
     instagram: "https://instagram.com/brandbuzz",
     linkedin: "https://linkedin.com/company/brandbuzz",
